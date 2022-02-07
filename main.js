7582be6e58559159146f51d7a59eb596522d4245
david83748 <david.vashchenko@gmail.com>
2022-02-07 18:35:16 +0200

    add disct...

--- main.js
+++ main.js
@@ -10,44 +10,44 @@ const promise = new Promise ((resolve) => {
 });
 
 
-promise.then(data => render(data.location, data.current))
+promise.then(data => render(data))
 
-const render = (locaition, current) => {
+const render = ({location:{name, region, country, localtime}, current: {gust_kph, last_updated, temp_c, temp_f, condition:{icon, text}, wind_mph, wind_kph, humidity, cloud}}) => {
     document.getElementById('wrapper').insertAdjacentHTML(
       'beforeend',
       `
         <div class="main">
         
         <div class="main__item">
-        <div>Location: ${locaition.name}<div>
-        <div>Region: ${locaition.region}<div>
-        <div>Country: ${locaition.country}</div>
-        <div>Localtime: ${locaition.localtime}</div>
+        <div>Location: ${name}<div>
+        <div>Region: ${region}<div>
+        <div>Country: ${country}</div>
+        <div>Localtime: ${localtime}</div>
         <div>
         
         <hr>
         
         <div class="main__item">
-        <div>Gust: ${current.gust_kph} kph</div>
-        <div>Last updated: ${current.last_updated}</div>
-        <div>Сurrent temperature: ${current.temp_c}°C</div>
-        <div>Сurrent temperature: ${current.temp_f}°F</div>
+        <div>Gust: ${gust_kph} kph</div>
+        <div>Last updated: ${last_updated}</div>
+        <div>Сurrent temperature: ${temp_c}°C</div>
+        <div>Сurrent temperature: ${temp_f}°F</div>
         <div>
         
         <hr>
         
         <div class="main__item">
-        <div><img src="${current.condition.icon}" alt="rain"></div>
-        <div>${current.condition.text}</div>
+        <div><img src="${icon}" alt="rain"></div>
+        <div>${text}</div>
         <div>
         
         <hr>
         
         <div class="item">
-        <div>Wind: ${current.wind_mph}mph</div>
-        <div>Wind: ${current.wind_kph} kph</div>
-        <div>Humidity: ${current.humidity}%</div>
-        <div>Cloud: ${current.cloud}%</div>
+        <div>Wind: ${wind_mph}mph</div>
+        <div>Wind: ${wind_kph} kph</div>
+        <div>Humidity: ${humidity}%</div>
+        <div>Cloud: ${cloud}%</div>
         <div>
         
         <div>
